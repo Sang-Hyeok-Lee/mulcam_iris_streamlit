5229a1f57eaba5574adba3354a63f1fbf3be094e
anio <kitbung0214@gmail.com>
2023-09-01 11:22:58 +0900

    ml

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import streamlit as st
 from eda_app import run_eda_app
+from ml_app import run_ml_app
 
 def main():
     st.markdown("# Hello World")
@@ -14,7 +15,8 @@ def main():
         # st.subheader("탐색적 자료 분석")
         run_eda_app()
     elif choice =="머신러닝" :
-        st.subheader("머신러닝")
+        # st.subheader("머신러닝")
+        run_ml_app()
     elif choice =="About" :
         st.subheader("About")
     else:
