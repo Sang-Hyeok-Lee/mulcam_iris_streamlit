a246ebdc17fbd9c11bdf48227e7a0641e50049da
anio <kitbung0214@gmail.com>
2023-09-01 10:48:12 +0900

    a

--- eda_app.py
+++ eda_app.py
@@ -11,4 +11,18 @@ def run_eda_app():
 
     st.subheader("EDA")
     st.subheader("잘 진행중임!")
-    st.subheader("왜 안나오지")
+    
+    iris_df = pd.read_csv("data/iris.csv")
+    # 메뉴 지정
+    submenu=st.sidebar.selectbox("submenu", ["통계", "시각화", "그래프"])
+    if submenu == "통계":
+        st.subheader("통계")
+    elif submenu =="시각화" :
+        st.subheader("시각화")
+    elif submenu =="그래프" :
+        st.subheader("그래프")
+    else:
+        pass
+
+
+    st.dataframe(iris_df)
